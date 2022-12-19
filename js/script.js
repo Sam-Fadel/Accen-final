84117adc918df2558e520fef4275463da6d394c1
Sam97 <ausa0006@stud.kea.dk>
2022-12-20 00:40:00 +0100

    the final version of the website view

---
+++ js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", getData);
+
+const endpoint =
+  "https://tripoli.dk/Vivian-backend-101/wp-json/wp/v2/course?_embed&per_page=3";
+
+function getData() {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then(handleData);
+}
+
+function handleData(posts) {
+  posts.forEach(showcourses); //looping through all courses
+}
+
+function showcourses(course) {
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+
+  copy.querySelector("h2").textContent = course.title.rendered;
+  copy.querySelector("p").textContent = course.content.rendered;
+
+  copy.querySelector("img").src =
+    course._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+
+  document.querySelector(".fetching-menu").appendChild(copy);
+}
